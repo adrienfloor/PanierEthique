67378afd2d52d1082ef63954c7ff1cfda09671f2
Teddy Wing <git@teddywing.com>
2017-05-20 22:53:29 +0200

    Add Producer
    Table to store producer information and separate it from other data.
    Also to not hard-code it.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520122641) do
+ActiveRecord::Schema.define(version: 20170520203309) do
+
+  create_table "producers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"

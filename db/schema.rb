40f89e7069d2ca15a906938bbd7fad98e3cd6843
Teddy Wing <git@teddywing.com>
2017-05-20 23:06:06 +0200

    Add `status` field to `products` table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520203309) do
+ActiveRecord::Schema.define(version: 20170520210419) do
 
   create_table "producers", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170520203309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "producer_id"
+    t.string "status"
     t.index ["producer_id"], name: "index_products_on_producer_id"
   end
 

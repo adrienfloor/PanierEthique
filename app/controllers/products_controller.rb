5ba9264f2ac1bf3ae47c88d3bc8aff2fa72b6b36
adrienfloor <adriaandrum@gmail.com>
2017-05-20 18:18:01 +0200

    index created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,14 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+  def create
+    @product = Product.create(product_params)
+  end
+  def index
+    @products = Product.order(created_at: :desc).all
+  end
+  private
+  def product_params
+    params.require(:product).permit(:name, :quantity, :unit, :sale_price)
+  end
 end

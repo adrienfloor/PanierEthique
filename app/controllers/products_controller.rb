da73f9df9be0a2dc16621469c16b02b4e25aca83
adrienfloor <adriaandrum@gmail.com>
2017-05-20 18:43:48 +0200

    redirection to index after submiting on product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,12 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
   def create
-    @product = Product.create(product_params)
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
   def index
     @products = Product.order(created_at: :desc).all
